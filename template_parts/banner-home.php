8063690d6a16b17e025794c9d3272935dca4c651
olaormundos <vanderleidasilvaormundo@gmail.com>
2023-03-14 23:17:30 -0300

    header e banner 60%

---
+++ template_parts/banner-home.php
@@ -0,0 +1,12 @@
+<section>
+  <div class="banner">
+    <div class="container">
+      <div class="col-sm-5">
+        <img src="<?php echo get_template_directory_uri().'/assets/images/heroimage.png'?>" alt="" class="hero-image">
+      </div>
+      <div class="col-sm-7">
+
+      </div>
+    </div>
+  </div>
+</section> 
