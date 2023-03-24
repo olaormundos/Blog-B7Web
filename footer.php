6d09fea5c05a7a279617c97f62be25fada598e99
olaormundos <vanderleidasilvaormundo@gmail.com>
2023-03-24 16:48:02 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+            <footer>
+                <div class="footer_down">
+                    <div class="container">
+                        Criado com ❤️ por OlaOrmundos
+                    </div>
+                </div>
+            </footer>
             <script type="text/javascript">
                 var ajaxUrl = "<?php echo admin_url('admin-ajax.php'); ?>";
             </script>
